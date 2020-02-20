f6de6543282550d797234a6792ea506d021a62dc
Ali Can Gönüllü <alicangonullu1907@gmail.com>
2020-02-21 00:10:10 +0300

    macOS Start File

--- kvm-basla.sh
+++ kvm-basla.sh
@@ -21,6 +21,7 @@ sudo qemu-system-x86_64 \
     -device e1000-82545em,netdev=net0,id=net0,mac=52:54:00:c9:18:27 \
     -device ich9-ahci,id=sata \
     -drive id=ESP,if=none,format=qcow2,file=ESP.qcow2 \
+    -drive id=macOS,if=none,format=qcow2,file=MyDisk.qcow2 \
     -device ide-hd,bus=sata.2,drive=ESP \
     -drive id=InstallMedia,format=raw,if=none,file=BaseSystem.img \
     -device ide-hd,bus=sata.3,drive=InstallMedia \
