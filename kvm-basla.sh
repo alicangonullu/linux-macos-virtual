06c64dbc902025665f3e5e1b6cd4587cb063000a
Ali Can Gönüllü <alicangonullu1907@gmail.com>
2020-02-21 00:21:29 +0300

    macOS KVM Start File

--- kvm-basla.sh
+++ kvm-basla.sh
@@ -2,27 +2,54 @@
 OSK="ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc"
 VMDIR=$PWD
 OVMF=$VMDIR/firmware
-#export QEMU_AUDIO_DRV=pa
-#QEMU_AUDIO_DRV=pa
-sudo qemu-system-x86_64 \
+
+export QEMU_AUDIO_DRV=pa
+QEMU_AUDIO_DRV=pa
+SYSTEM_DISK="MyDisk.qcow2"
+
+[[ -z "$MEM" ]] && {
+	MEM="2G"
+}
+
+[[ -z "$CPUS" ]] && {
+	CPUS=2
+}
+
+[[ -z "$SYSTEM_DISK" ]] && {
+    echo "SYSTEM_DISK belirleyiniz"
+    exit 1
+}
+
+[[ -r "$SYSTEM_DISK" ]] || {
+    echo "Sistem Disk Okunamıyor: $SYSTEM_DISK"
+    exit 1
+}
+
+MOREARGS=()
+
+[[ "$HEADLESS" = "1" ]] && {
+    MOREARGS+=(-nographic -vnc :0 -k en-us)
+}
+
+qemu-system-x86_64 \
     -enable-kvm \
-    -m 2G \
+    -m $MEM \
     -machine q35,accel=kvm \
-    -smp 4,cores=2 \
+    -smp $CPUS \
     -cpu Penryn,vendor=GenuineIntel,kvm=on,+sse3,+sse4.2,+aes,+xsave,+avx,+xsaveopt,+xsavec,+xgetbv1,+avx2,+bmi2,+smep,+bmi1,+fma,+movbe,+invtsc \
     -device isa-applesmc,osk="$OSK" \
     -smbios type=2 \
     -drive if=pflash,format=raw,readonly,file="$OVMF/OVMF_CODE.fd" \
     -drive if=pflash,format=raw,file="$OVMF/OVMF_VARS-1024x768.fd" \
     -vga qxl \
-    -device ich9-intel-hda -device hda-output \
-    -usb -device usb-kbd -device usb-mouse \
+    -usb -device usb-kbd -device usb-tablet \
     -netdev user,id=net0 \
-    -device e1000-82545em,netdev=net0,id=net0,mac=52:54:00:c9:18:27 \
+    -device e1000-82545em,netdev=net0,id=net0,mac=52:54:00:0e:0d:20 \
     -device ich9-ahci,id=sata \
     -drive id=ESP,if=none,format=qcow2,file=ESP.qcow2 \
-    -drive id=SistemDisk,if=none,file=MyDisk.qcow2 \
-    -device ide-hd,bus=sata.4,drive=SistemDisk \
     -device ide-hd,bus=sata.2,drive=ESP \
     -drive id=InstallMedia,format=raw,if=none,file=BaseSystem.img \
     -device ide-hd,bus=sata.3,drive=InstallMedia \
+    -drive id=SystemDisk,if=none,file="${SYSTEM_DISK}" \
+    -device ide-hd,bus=sata.4,drive=SystemDisk \
+    "${MOREARGS[@]}"
