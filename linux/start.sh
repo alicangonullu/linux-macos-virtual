eba8b0e3ceb2ba2ddbec905160abf6bf31405282
Ali Can Gönüllü <alicangonullu1907@gmail.com>
2020-10-29 01:21:19 +0200

    Update start.sh

--- linux/start.sh
+++ linux/start.sh
@@ -6,41 +6,23 @@ apt update
 sudo apt-get install qemu-system qemu-utils python3 python3-pip
 clear
 
-kullanim_goster() {
-    echo
-    echo "Kullanım: $0"
-    echo
-    echo " -y, --yardim        Yardım."
-    echo " -s, --high-sierra   High Sierra Kurulumu."
-    echo " -m, --mojave        Mojave Kurulumu."
-    echo " -c, --catalina      Catalina Kurulumu."
-    echo " -u, --unknown      Catalina Kurulumu."
-    echo
-}
-
-error() {
-    local error_message="$*"
-    echo "${error_message}" 1>&2;
-}
-
 argument="$1"
-case $argument in
-    -y|--yardim)
-        kullanim_goster
-        ;;
-    -s|--high-sierra)
-        "$TOOLS/FetchMacOS/fetch.sh" -v 10.13 || exit 1;
-        ;;
-    -m|--mojave)
-        "$TOOLS/FetchMacOS/fetch.sh" -v 10.14 || exit 1;
-        ;;
-    -c|--catalina|*)
-        "$TOOLS/FetchMacOS/fetch.sh" -v 10.15 || exit 1;
-        ;;
-     -u|--unknown|*)
-        "$TOOLS/FetchMacOS/fetch.sh" -v 10.16 || exit 1;
-        ;;
-esac
+
+
+echo -n "c = Catalina / h = High Sierra / m = Mojave / u = Unknown"
+read selectos
+
+if [ "$selectos" != "${selectos#[Hh]}" ] ;then
+"$TOOLS/FetchMacOS/fetch.sh" -v 10.13 || exit 1;
+elif [ "$selectos" != "${selectos#[Mm]}" ] ;then
+"$TOOLS/FetchMacOS/fetch.sh" -v 10.14 || exit 1;
+elif [ "$selectos" != "${selectos#[Cc]}" ] ;then
+"$TOOLS/FetchMacOS/fetch.sh" -v 10.15 || exit 1;
+elif [ "$selectos" != "${selectos#[Uu]}" ] ;then
+"$TOOLS/FetchMacOS/fetch.sh" -v 10.16 || exit 1;
+else
+  exit 1;
+fi
 
 "$TOOLS/dmg2img" "$TOOLS/FetchMacOS/BaseSystem/BaseSystem.dmg" "$PWD/BaseSystem.img"
 
